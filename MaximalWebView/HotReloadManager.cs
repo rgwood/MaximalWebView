4785298354d9c2e5c36da90d6cd3ba77568f71d2
Reilly Wood <reilly.wood@icloud.com>
2021-11-06 20:09:25 -0700

    Add environment variable check to determine whether hot reload is enabled

--- MaximalWebView/HotReloadManager.cs
+++ MaximalWebView/HotReloadManager.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using System.Reflection.Metadata;
+﻿using System.Reflection.Metadata;
 using MaximalWebView;
-using System.Diagnostics;
 
 [assembly: MetadataUpdateHandler(typeof(HotReloadManager))]
 
 namespace MaximalWebView;
+
 internal static class HotReloadManager
 {
     public static void ClearCache(Type[]? types)
@@ -21,5 +15,17 @@ internal static class HotReloadManager
     public static void UpdateApplication(Type[]? types)
     {
         Console.WriteLine($"UpdateApplication called for {types?.Length ?? 0} type(s): {string.Join(';', types?.Select(t => t.Name) ?? Array.Empty<string>())}");
+    }
+
+    /// <summary>
+    /// Best-effort guess at whether hot reload is enabled. Useful for determining whether to enable our own custom hot reload features.
+    /// </summary>
+    public static bool IsHotReloadEnabled()
+    {
+        // Using the DOTNET_MODIFIABLE_ASSEMBLIES env var introduced here: https://github.com/dotnet/runtime/issues/47274
+        // There might be better ways to do this, but custom hot reload scenarios aren't really documented yet.
+        // Keep an eye on this space.
+        string? modifiableAssembliesVar = Environment.GetEnvironmentVariable("DOTNET_MODIFIABLE_ASSEMBLIES");
+        return modifiableAssembliesVar != null && modifiableAssembliesVar.Equals("debug", StringComparison.OrdinalIgnoreCase);
     }
 }
